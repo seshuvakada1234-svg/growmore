8610f02815d5eed4a18cc55332b8e21c669299c3
seshu vakada <seshuvakada1234@gmail.com>
2026-03-02 10:16:24 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-935976831-d4bc4",
+  "appId": "1:249420871101:web:00992fa45f2508f3360ed4",
+  "apiKey": "AIzaSyDZbs42kOzlZQhRbpDzlKh0xSZXqYyrTBg",
+  "authDomain": "studio-935976831-d4bc4.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "249420871101"
+};
